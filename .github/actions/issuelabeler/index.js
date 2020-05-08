9d82bcc2a54820bb77c7600cad16c39dc4e59c3d
Coruna <51291572+Coruna@users.noreply.github.com>
2020-05-08 14:40:16 +0200

    Update index.js

---
+++ .github/actions/issuelabeler/index.js
@@ -0,0 +1,28 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+async function run() {
+  try {
+  
+    const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+    const token = core.getInput("repo-token");
+    const number = core.getInput("number");
+    const octokit = new github.GitHub(token);
+    
+    const label = "new"
+        
+    // Label issue
+    const response = await octokit.issues.addLabels({
+      repo,
+      issue_number: number,
+      labels: label
+    });      
+       
+    console.log(response);
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
